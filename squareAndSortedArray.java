74322341265cda2eca0b2bbc3042b72c21a1e0c6
DEBADATTAJENA <75190799+DEBADATTAJENA@users.noreply.github.com>
2021-01-23 09:51:49 +0530

    Add files via upload

--- squareAndSortedArray.java
+++ squareAndSortedArray.java
@@ -1,37 +1,21 @@
 package hearth;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class squareAndSortedArray {
 
 	public static void main(String[] args) {
 		
-	      int [] arr = {-3,0,4,-1,2};
+	      int [] arr = {-4,-1,0,3,10};
 	      int square = 0;
 	      int temp = 0;
-	      ArrayList<Integer> al = new ArrayList<>(arr.length);
-	      
-	     for (int i = 0 ; i <arr.length;i++) {
-	    	 square  = arr[i]*arr[i];
-	     
-	     al.add(square);
-	     }
-	   Integer[] sarr = new Integer[al.size()];
-	   sarr = al.toArray(sarr);
-	  
-	    	for(int i=0;i<sarr.length;i++) {
-	    		for(int j =i+1;j<sarr.length;j++) {
-	    			if(sarr[i]>sarr[j]) {
-	    				temp = sarr[i];
-	    				sarr[i]=sarr[j];
-	    				sarr[j] = temp;
-	    			}
-	    		}
-	    		System.out.print(sarr[i]+", ");
-	    		
-	    		
+	     int [] sarr = new int [arr.length];
+	    	for(int i=0;i<arr.length;i++) {
+	    		square = arr[i]*arr[i];
+	    		sarr[i] = square;
 	    	}
-	     
-	     }
+	    	Arrays.sort(sarr);
+	     System.out.print(Arrays.toString(sarr));
+	}
 }
